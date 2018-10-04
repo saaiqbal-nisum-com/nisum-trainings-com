de5f8ef0d5ec8aceda9fb63a1eecc49c61666821
Saad Iqbal <saaiqbal@nisum.com>
2018-10-04 23:00:01 +0500

    PNT-379

--- src/main/java/com/nisum/TrainingApplication.java
+++ src/main/java/com/nisum/TrainingApplication.java
@@ -2,11 +2,18 @@ package com.nisum;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class TrainingApplication {
+public class TrainingApplication extends SpringBootServletInitializer	 {
 
 	public static void main(String[] args) throws Exception{
 		SpringApplication.run(TrainingApplication.class, args);
 	}
+	
+	 @Override
+	    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+	        return application.sources(TrainingApplication.class);
+	    }   
 }
