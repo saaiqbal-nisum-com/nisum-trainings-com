7ea4f295cadf28d5cb0eea19707530cf5a9a9f22
Adeel <ayounis@nisum.com>
2018-10-04 11:41:50 +0500

    reverse commit

---
+++ src/main/java/com/nisum/TrainingApplication.java
@@ -0,0 +1,12 @@
+package com.nisum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainingApplication {
+
+	public static void main(String[] args) throws Exception{
+		SpringApplication.run(TrainingApplication.class, args);
+	}
+}
